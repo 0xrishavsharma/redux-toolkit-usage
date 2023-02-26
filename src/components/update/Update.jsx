ed9be522554ad0ffa7bab1899a13c646623bb144
Rishav Sharma <rishavtosharma@gmail.com>
2023-02-26 07:35:56 +0530

    Cloning and Initialization of project

--- src/components/update/Update.jsx
+++ src/components/update/Update.jsx
@@ -1,25 +1,16 @@
-import React, { useContext } from "react";
+import React from "react";
 import Warning from "../warning/Warning";
 import "./update.css";
-// import { update, remove } from "../../redux/userSlice";
-import { useSelector } from "react-redux";
-import { useDispatch } from "react-redux";
 import { useState } from "react";
-import { updateUser2 } from "../../redux/userSlice";
 
 export default function Update() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
-  const user = useSelector((state) => state.user);
-  const dispatch = useDispatch();
 
-  const handleClick = (e) => {
-    e.preventDefault();
-    // without API
-    // dispatch(update({ name, email }));
-    // with API
-    dispatch(updateUser2({ name, email }));
-  };
+
+  console.log(name)
+  console.log(email)
+
 
   return (
     <div className="update">
@@ -45,7 +36,7 @@ export default function Update() {
               <input
                 className="formInput"
                 type="text"
-                placeholder={user.user.name}
+                placeholder="John"
                 onChange={(e) => setName(e.target.value)}
               />
             </div>
@@ -54,25 +45,19 @@ export default function Update() {
               <input
                 className="formInput"
                 type="text"
-                placeholder={user.user.email}
+                placeholder="john@gmail.com"
                 onChange={(e) => setEmail(e.target.value)}
               />
             </div>
-            <div className="formItem">
+            {/* <div className="formItem">
               <label>Password</label>
               <input className="formInput" type="password" />
-            </div>
+            </div> */}
             <button
-              disabled={user.pending}
               className="updateButton"
-              onClick={handleClick}
             >
               Update
             </button>
-            {user.error && <span className="error">Something went wrong!</span>}
-            {user.pending === false && (
-              <span className="success">Account has been updated!</span>
-            )}
           </form>
         </div>
       </div>
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
-import userReducer from "./userSlice";
 
-export default configureStore({
-  reducer: {
-    user: userReducer,
-  },
-  // reducer: {
-  //   user: userReducer,
-  //   post: postReducer,
-  // },
-});
+// export const store = () => {
+
+// }
+
